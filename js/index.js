5f7906d4453b685ccde364cdf760870fdc581b89
Mac Pro <mpro@PAR-222-DR.local>
2016-01-05 11:32:08 -0700

    Timeline controls are functional. Timeline styling.

--- js/index.js
+++ js/index.js
@@ -5,6 +5,10 @@
  */
 $(function() {
 
+  // Pause the timeline on load
+  $("#pause").addClass('focused');
+
+
   // Load TimelineMax
   var tl = new TimelineMax( {onUpdate:updateDragger, paused:true} );
 
@@ -24,6 +28,7 @@ $(function() {
   function updateDragger() {
     TweenLite.set(dr, {x: wi * tl.progress()});
     ti.html(tl.time().toFixed( 2 ));
+    endChecker();
   }
 
   /** Create the dragger with the "Draggable" plugin.
@@ -34,11 +39,64 @@ $(function() {
     bounds: {minX: 0, maxX: wi},
     onDrag: function(){
       tl.progress(this.x / tr).pause();
+      if (!$('#pause').hasClass('focused')){
+          $('#pause').addClass('focused')
+      }
+      endChecker();
     }
   });
+  // Hotkeys
+  $('body').keyup(function(e){
+   e.preventDefault();
+   if(e.keyCode == 32){
+       // user has pressed space
+       toggleControls();
+   }
+});
 
-  $( "#play" ).click(function(){ tl.play(); });
-  $( "#pause" ).click(function(){ tl.pause(); });
+  // Handle the control buttons
+  var toggleControls = function (){
+    // playing -> pause
+    if (!$('#pause').hasClass('focused')){
+        $('#pause').removeClass('unfocused');
+        $('#play').removeClass('focused');
+
+        $('#pause').addClass('focused');
+        $('#play').addClass('unfocused');
+        tl.pause();
+    }
+
+    //paused -> playing
+    else if ($('#pause').hasClass('focused')){
+        $('#pause').removeClass('focused')
+        $('#play').removeClass('unfocused')
+
+        $('#play').addClass('focused')
+        $('#pause').addClass('unfocused')
+        tl.play();
+    }
+  }
+
+  $( "#play" ).click(function(){
+        tl.play();
+        toggleControls();
+   });
+
+  $( "#pause" ).click(function(){
+    tl.pause();
+    toggleControls();
+  });
   // $( "#seek" ).click(function(){ tl.seek(1.5); });
+
+var endChecker = function(){
+  if (tl.progress() === 1){
+    tl.pause();
+    $('#pause').removeClass('unfocused');
+    $('#play').removeClass('focused');
+
+    $('#pause').addClass('focused');
+    $('#play').addClass('unfocused');
+  }
+}
 
 });
