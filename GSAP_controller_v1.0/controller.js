86cad7431aeeba5839fff2a1de895c9d475ecffa
zfalen <mpro@PAR-222-DR.local>
2016-03-15 11:10:33 -0600

    update to remove bootstrap dependency

--- GSAP_controller_v1.0/controller.js
+++ GSAP_controller_v1.0/controller.js
@@ -11,7 +11,7 @@
 
  // Render the controller to the appropriate div
  $('#GSAPcontroller').append(
-  '<div id="controller" class="controllerContainer controllerHolder"> <div class="row"> <div class="col-md-9"> <div id="trackContainer" class="controllerTimeline"> <p id="controllerTime">0.00</p> <div class="dragger-track"> <div id="controllerPlayhead"> <div></div> </div> </div> </div> </div> <div class="col-md-3"> <div class="controllerButtons"> <button id="play"><i class="fa fa-2x fa-play-circle-o"></i></button> <button id="pause"><i class="fa fa-2x fa-pause-circle-o"></i></button> </div> </div> </div> <div class="row"> <div class="center-block" style="margin-top: 5px; text-align: right; padding-right: 225px;"> <p class="controllerLogoText"> a widget by </p> <a href="http://www.partnerscreative.com"> <img src="./GSAP_controller_v1.0/par_logo.svg" class="img-responsive controllerLogo"> </a> </div> </div> </div>'
+  '<div id="controller" class="controllerContainer controllerHolder"> <div class="row"> <div class="col-md-9"> <div id="trackContainer" class="controllerTimeline"> <p id="controllerTime">0.00</p> <div class="dragger-track"> <div id="controllerPlayhead"> <div></div> </div> </div> </div> </div> <div class="col-md-3"> <div class="controllerButtons"> <button id="play"><i class="fa fa-2x fa-play-circle-o"></i></button> <button id="pause"><i class="fa fa-2x fa-pause-circle-o"></i></button> </div> </div> </div> <div class="row"> <div class="center-block" style="margin-top: 5px; text-align: right; padding-right: 90px;"> <p class="controllerLogoText"> a widget by </p> <a href="http://www.partnerscreative.com"> <img src="./GSAP_controller_v1.0/par_logo.svg" class="img-responsive controllerLogo"> </a> </div> </div> </div>'
    )
 
  // Pause the timeline on load
