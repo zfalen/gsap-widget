d97cee382f903fb5d313b189e151407fcf7f67ba
zfalen <mpro@PAR-222-DR.local>
2016-03-16 14:26:12 -0600

    Final updates to the Guides widget

---
+++ DevBanner_728x90/scripts/script.js
@@ -0,0 +1,25 @@
+var adDiv;
+
+function initEB() {
+    if (!EB.isInitialized()) {
+        EB.addEventListener(EBG.EventName.EB_INITIALIZED, startAd);
+    } else {
+        startAd();
+    }
+}
+
+function startAd() {
+    adDiv = document.getElementById("ad");
+
+    addEventListeners();
+}
+
+function addEventListeners() {
+    document.getElementById("ad").addEventListener("click", clickthrough);
+}
+
+function clickthrough() {
+    EB.clickthrough();
+}
+
+window.addEventListener("load", initEB);
